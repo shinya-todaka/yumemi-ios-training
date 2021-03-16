7d97ad8b0a886be333f1989d37e2f3a3c1e4cca7
shinya-todaka <tdk0105.strawberry@gmail.com>
2021-03-16 12:28:53 +0900

    fix indent

--- yumemi-ios-training/SceneDelegate.swift
+++ yumemi-ios-training/SceneDelegate.swift
@@ -17,14 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else {
-                  return
-              }
-          let window = UIWindow(windowScene: scene)
-          self.window = window
-          window.makeKeyAndVisible()
+            return
+        }
+        let window = UIWindow(windowScene: scene)
+        self.window = window
+        window.makeKeyAndVisible()
 
-          let weatherViewController = WeatherViewController.instantiate()
-          window.rootViewController = weatherViewController
+        let weatherViewController = WeatherViewController.instantiate()
+        window.rootViewController = weatherViewController
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
