ed7181d1cde1e66ebca40341a7f0a4c2ef4c3bf4
shinya-todaka <tdk0105.strawberry@gmail.com>
2021-03-19 12:51:02 +0900

    fix space between case

--- yumemi-ios-training/Error/FetchWeatherError.swift
+++ yumemi-ios-training/Error/FetchWeatherError.swift
@@ -16,6 +16,7 @@ enum FetchWeatherError: Error {
         switch self {
         case .apiError(.invalidParameterError):
             return "パラメータが正しくありません"
+            
         case .apiError(.jsonDecodeError):
             return "JSONのデコードに失敗しました"
             
