e9f5f3a44198b388cbf36e4d13219e5c7a07172e
shinya-todaka <tdk0105.strawberry@gmail.com>
2021-03-24 06:57:56 +0900

    remove unnecessary import

--- yumemi-ios-training/WeatherModel/WeatherModel.swift
+++ yumemi-ios-training/WeatherModel/WeatherModel.swift
@@ -5,8 +5,6 @@
 //  Created by 戸高 新也 on 2021/03/22.
 //
 
-import Foundation
-
 /// @mockable
 protocol WeatherModel {
     func fetchWeather(request: WeatherRequest) -> Result<WeatherInfo, FetchWeatherError>
