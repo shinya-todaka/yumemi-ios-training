06ec5a46cb15688de5d47ea176206986aa925b96
shinya-todaka <tdk0105.strawberry@gmail.com>
2021-03-24 06:49:41 +0900

    add mockolo via mint

---
+++ Scripts/XcodeGen/mockolo.sh
@@ -0,0 +1,10 @@
+if which mint >/dev/null; then
+  rm -f $SRCROOT/$TARGET_NAME/Generated/MockResults.swift
+  xcrun --sdk macosx mint run uber/mockolo mockolo --sourcedirs $SRCROOT/yumemi-ios-training \
+						   --destination $SRCROOT/$TARGET_NAME/Generated/MockResults.swift \
+						   --testable-imports yumemi_ios_training \
+						   --mock-final
+					          
+else
+  echo "warning: Mint not installed, download from https://github.com/yonaskolb/Mint"
+fi
